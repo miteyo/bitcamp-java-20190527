09fb4a03878d8c7bc7d49e415e1fce2b11e19622
youngaeom <eomduddk@gmail.com>
2019-08-07 16:42:19 +0900

    bitcamp-java-application v34_2 완료

---
+++ bitcamp-java-application-server/v34_2/src/main/java/com/eomcs/lms/dao/MemberDao.java
@@ -0,0 +1,19 @@
+package com.eomcs.lms.dao;
+
+import java.util.List;
+import com.eomcs.lms.domain.Member;
+
+// 회원관리 DAO 규칙을 정의한다.
+public interface MemberDao {
+
+  int insert(Member member) throws Exception;
+
+  List<Member> findAll() throws Exception;
+
+  Member findBy(int no) throws Exception;
+
+  int update(Member member) throws Exception;
+
+  int delete(int no) throws Exception;
+
+}
